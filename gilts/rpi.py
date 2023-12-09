05923e9736b14dffb3c36542eb3524fa82367c66
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2023-12-09 09:31:25 +0000

    Refactor RPI.

--- gilts/rpi.py
+++ gilts/rpi.py
@@ -52,80 +52,78 @@ class OutOfDateError(Exception):
     pass
 
 
-ref_year = 1987
-
-
-@caching.cache_data(ttl=24*3600)
-def parse(filename, verbosity=0, ignore_date=False):
-    stream = open(filename, 'rt')
-    series = []
-    next_year = ref_year
-    next_month = 1
-    for row in csv.reader(stream):
-        if verbosity >= 2:
-            print(row)
-        if len(row) != 2:
-            continue
-        date, value = row
-        if date == "Next release":
-            mo = _next_release_re.match(value)
-            assert mo
-            year = int(mo.group('year'))
-            month = _long_months.index(mo.group('month')) + 1
-            day = int(mo.group('day'))
-            next_release = datetime.date(year, month, day)
-            if datetime.datetime.utcnow().date() > next_release:
-                logger.warning(f'{filename} has been superseded on {next_release}')
-                raise OutOfDateError
-        mo = _monthly_re.match(date)
-        if not mo:
-            continue
-        year = int(mo.group('year'))
-        month = _months.index(mo.group('month')) + 1
-        date = datetime.date(year, month, 1)
-        value = float(value)
-        if verbosity >= 1:
-            print(f'{date}, {value:.1f}')
-        assert year == next_year
-        assert month == next_month
-        series.append(value)
-        next_year += next_month // 12
-        next_month = next_month % 12 + 1
-    return series
-
-
-_url = 'https://www.ons.gov.uk/generator?format=csv&uri=/economy/inflationandpriceindices/timeseries/chaw/mm23'
-
-
-def load():
-    filename = os.path.join(os.path.dirname(__file__), 'rpi-series.csv')
-    try:
-        return parse(filename)
-    except (FileNotFoundError, OutOfDateError):
-        download(_url, filename)
-        return parse(filename)
-
-
 # https://www.ons.gov.uk/economy/inflationandpriceindices/timeseries/chaw/mm23
 class RPI:
 
-    def __init__(self):
-        self.series = load()
+    ref_year = 1987
+
+    def __init__(self, filename=None):
+        if filename is None:
+            self.series = self._load()
+        else:
+            self.series = self._parse(filename, ignore_date=True)
         assert self.series
+
+    _url = 'https://www.ons.gov.uk/generator?format=csv&uri=/economy/inflationandpriceindices/timeseries/chaw/mm23'
+
+    @classmethod
+    def _load(cls):
+        filename = os.path.join(os.path.dirname(__file__), 'rpi-series.csv')
+        try:
+            return cls._parse(filename)
+        except (FileNotFoundError, OutOfDateError):
+            download(RPI._url, filename)
+            return cls._parse(filename)
 
     def last_date(self):
         months = len(self.series) - 1
-        year = ref_year + months // 12
+        year = self.ref_year + months // 12
         month = months % 12 + 1
         return datetime.date(year, month, 1)
 
+    @staticmethod
+    @caching.cache_data(ttl=24*3600)
+    def _parse(filename, verbosity=0, ignore_date=False):
+        stream = open(filename, 'rt')
+        series = []
+        next_year = RPI.ref_year
+        next_month = 1
+        for row in csv.reader(stream):
+            if verbosity >= 2:
+                print(row)
+            if len(row) != 2:
+                continue
+            date, value = row
+            if date == "Next release":
+                mo = _next_release_re.match(value)
+                assert mo
+                year = int(mo.group('year'))
+                month = _long_months.index(mo.group('month')) + 1
+                day = int(mo.group('day'))
+                next_release = datetime.date(year, month, day)
+                if datetime.datetime.utcnow().date() > next_release:
+                    logger.warning(f'{filename} has been superseded on {next_release}')
+                    raise OutOfDateError
+            mo = _monthly_re.match(date)
+            if not mo:
+                continue
+            year = int(mo.group('year'))
+            month = _months.index(mo.group('month')) + 1
+            date = datetime.date(year, month, 1)
+            value = float(value)
+            if verbosity >= 1:
+                print(f'{date}, {value:.1f}')
+            assert year == next_year
+            assert month == next_month
+            series.append(value)
+            next_year += next_month // 12
+            next_month = next_month % 12 + 1
+        return series
 
     # https://www.dmo.gov.uk/media/0ltegugd/igcalc.pdf
-    #https://www.dmo.gov.uk/media/0ltegugd/igcalc.pdf
-
     def _index(self, date):
-        assert date.year >= ref_year
-        return (date.year - ref_year)*12 + (date.month - 1)
+        assert date.year >= self.ref_year
+        return (date.year - self.ref_year)*12 + (date.month - 1)
 
     # https://www.dmo.gov.uk/media/1sljygul/yldeqns.pdf,
     # Annex B: Method of indexation for index-linked gilts with a 3-month indexation lag
@@ -139,7 +137,7 @@ class RPI:
         return rpi
 
     def value(self, date):
-        assert date.year >= ref_year
+        assert date.year >= self.ref_year
 
         idx = self._index(date)
         rpi0 = self.series[idx]
@@ -167,3 +165,5 @@ class RPI:
         rpi1 = self._extrapolate(idx + 1, inflation_rate)
 
         return self._interpolate(date, rpi0, rpi1)
+
+
