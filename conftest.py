e6733c1fdeeb41dd7df0991dc62711303ae98d11
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2023-12-21 19:47:25 +0000

    Add a Selenium based test.

--- conftest.py
+++ conftest.py
@@ -13,8 +13,14 @@ pytest.register_assert_rewrite("gilts")
 
 def pytest_addoption(parser):
     parser.addoption("--show-plots", action="store_true", default=False, help="Show plots")
+    parser.addoption("--production", action="store_true")
 
 
 @pytest.fixture
 def show_plots(request):
     return request.config.getoption("--show-plots")
+
+
+@pytest.fixture(scope="session")
+def production(request):
+    return request.config.getoption("--production")
---
+++ gilts/test_selenium.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2023 LateGenXer
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+
+
+import os.path
+import pickle
+import socket
+import subprocess
+import time
+
+import pytest
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(scope="session")
+def server(production, worker_id):
+    if production:
+        # XXX Avoid the iframe
+        yield 'https://lategenxer.streamlit.app/~/+'
+        return
+
+    host = '127.0.0.1'
+    port = 8501 + (hash(worker_id) % 100)
+
+    app = subprocess.Popen([
+        'pipenv', 'run',
+        'streamlit', 'run',
+        '--server.address', host,
+        '--server.port', str(port),
+        '--server.headless', 'true',
+        '--',
+        'Home.py'
+    ], stdout=subprocess.DEVNULL)
+
+    # https://stackoverflow.com/a/19196218
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tries = 300
+    result = sock.connect_ex((host, port))
+    while result != 0 and tries > 0:
+        tries -= 1
+        time.sleep(0.010)
+        result = sock.connect_ex((host, port))
+    sock.close()
+    print(tries)
+    if result != 0:
+        raise TimeoutError
+
+    try:
+        yield f'http://{host}:{port}'
+    finally:
+        app.terminate()
+        app.wait()
+
+
+headless = True
+
+
+@pytest.fixture(scope="session")
+def driver():
+    options = Options()
+    if headless:
+        options.add_argument('--headless=new')  #https://www.selenium.dev/blog/2023/headless-is-going-away/
+        options.add_argument('--window-size=1080,1920')
+        # https://stackoverflow.com/questions/45631715/downloading-with-chrome-headless-and-selenium/73840130#73840130
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+    try:
+        # https://github.com/actions/runner-images/blob/main/images/ubuntu/Ubuntu2204-Readme.md
+        executable_dir = os.environ['CHROMEWEBDRIVER']
+    except KeyError:
+        from webdriver_manager.chrome import ChromeDriverManager
+        executable_path = ChromeDriverManager().install()
+    else:
+        executable_path = os.path.join(executable_dir, "chromedriver")
+
+    service = Service(executable_path=executable_path, log_output="chromedriver.log")
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+def test_gilt_ladder(server, driver):
+    driver.get(server + '/Gilt_Ladder')
+
+    # Use the same analytics anonymous ID.
+    driver.add_cookie({
+        'name': 'ajs_anonymous_id',
+        'value': 'ac78b155-525b-4578-97f6-58aa2f2bf366'
+    })
+
+    driver.implicitly_wait(15)
+
+    driver.find_element(By.ID, 'disclaimer');
+    driver.implicitly_wait(0)
+
+    with pytest.raises(NoSuchElementException):
+        exception = driver.find_element(By.XPATH, "//div[@class='stException']");
+
+    if False:
+        index_linked = driver.find_element(By.XPATH, "//p[text()='Index-linked']");
+        index_linked.click()
+    else:
+        # https://www.selenium.dev/documentation/webdriver/elements/file_upload/
+        s = driver.find_element(By.XPATH, "//input[@type='file']")
+        s.send_keys(os.path.join(os.path.dirname(__file__), 'test_schedule.csv'))
+
+    time.sleep(3)
+
+    driver.implicitly_wait(15)
+    driver.find_element(By.ID, 'disclaimer');
+    driver.implicitly_wait(0)
+
+    with pytest.raises(NoSuchElementException):
+        exception = driver.find_element(By.XPATH, "//div[@class='stException']");
+        driver.save_screenshot('selenium.png')
