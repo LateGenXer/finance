026de090817f413c9d4b7db37f268416b5f09b80
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2024-08-16 11:32:42 +0100

    Cache RPI series.

--- .github/scripts/gh-pages.sh
+++ .github/scripts/gh-pages.sh
@@ -4,4 +4,5 @@ set -eux
 
 mkdir -p _site
 python download.py 'https://www.dmo.gov.uk/data/XmlDataReport?reportCode=D1A' _site/dmo-D1A.xml text/xml
+python download.py 'https://www.ons.gov.uk/generator?format=csv&uri=/economy/inflationandpriceindices/timeseries/chaw/mm23' _site/rpi-series.csv
 python lse.py > _site/gilts-closing-prices.csv
