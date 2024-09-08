fd9211773a516f6c82639edef8e19b2d7e063c01
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2024-09-08 17:54:58 +0100

    Fix print button screen position.

--- cgtcalc.py
+++ cgtcalc.py
@@ -263,12 +263,17 @@ class HtmlReport(Report):
   @page { margin: 0; }
   body { margin: 1.6cm; }
 }
+.fixed-right {
+  position: fixed;
+  top: 0;
+  right: 0;
+}
 </style>
 </head>
 <body>
 <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-<div class="pull-right hidden-print">
+<div class="fixed-right hidden-print">
 <button class="btn btn-primary" onclick="window.print()">Print</button>
 </div>
 <div class="container-fluid">
