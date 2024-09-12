5b5a8f3c86924530bc8db9d32fb0e96a2ad73e20
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2024-09-12 20:51:38 +0100

    Use full Statcounter script.

--- analytics.py
+++ analytics.py
@@ -15,11 +15,19 @@ import streamlit as st
 def html():
 
     html = (
+        '<script type="text/javascript">'
+        'var sc_project=13036387; '
+        'var sc_invisible=1; '
+        'var sc_security="3699fd22"; '
+        '</script>'
+        '<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>'
+        '<noscript>'
         '<div class="statcounter">'
         '<a title="Web Analytics Made Easy - Statcounter" href="https://statcounter.com/" target="_blank">'
         '<img class="statcounter" src="https://c.statcounter.com/13036387/0/3699fd22/1/" alt="Web Analytics Made Easy - Statcounter" referrerPolicy="no-referrer-when-downgrade">'
         '</a>'
         '</div>'
+        '</noscript>'
         if "PYTEST_CURRENT_TEST" not in os.environ else
         '<div class="statcounter">'
         '</div>'
