5deb1795fb48f0674b84a09940744c236194b4e3
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2024-09-19 20:35:51 +0100

    Justify disclaimer text.

--- pages/9_Disclaimer.py
+++ pages/9_Disclaimer.py
@@ -18,6 +18,8 @@ common.set_page_config(
 
 st.title('Disclaimer')
 
+st.html("<style>.stMarkdown { text-align: justify; }</style>")
+
 # https://termly.io/resources/templates/disclaimer-template/
 st.markdown('''
 The information provided by LateGenXer ('we', 'us', or 'our') on https://lategenxer.streamlit.app/ (the 'Site') is for general informational purposes only. All information on the Site is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness of any information on the Site. UNDER NO CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THE SITE OR RELIANCE ON ANY INFORMATION PROVIDED ON THE SITE. YOUR USE OF THE SITE AND YOUR RELIANCE ON ANY INFORMATION ON THE SITE IS SOLELY AT YOUR OWN RISK.
