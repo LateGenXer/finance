c2cfa05ce85bcb84e9ad96300f99a13b10a52c83
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2024-11-17 14:22:45 +0000

    Write cgtcalc.py version in report.
    Fixes https://github.com/LateGenXer/finance/issues/4

--- environ.py
+++ environ.py
@@ -1,9 +1,25 @@
 #
-# SPDX-License-Identifier: Unlicense
+# SPDX-License-Identifier: AGPL-3.0-or-later
 #
 
 
 import os
+import subprocess
 
 
 ci = os.environ.get('CI', 'false').lower() == 'true'
+
+
+def get_version():
+    try:
+        version = subprocess.check_output([
+            'git', 'show', '-s', '--date=format:%Y-%m-%d', '--format=%h (%cd)', 'HEAD',
+        ], text=True)
+    except subprocess.CalledProcessError:
+        if ci:
+            raise
+        else:
+            version = 'unknown'
+    else:
+        version = version.rstrip()
+    return version
