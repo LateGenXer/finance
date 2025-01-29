10d0a01b55203485e76ce377039566ea3b88a23a
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2025-01-29 18:59:23 +0000

    Fix git version when invoked out of tree.

--- environ.py
+++ environ.py
@@ -3,20 +3,26 @@
 #
 
 
-import os
+import os.path
 import subprocess
 
 
-ci = os.environ.get('CI', 'false').lower() == 'true'
+ci: bool = os.environ.get('CI', 'false').lower() == 'true'
 
 
-production = int(os.environ.get('PRODUCTION', '0'))
+production: bool = int(os.environ.get('PRODUCTION', '0')) != 0
 
 
-def get_version():
+def get_version() -> str:
     try:
         version = subprocess.check_output([
-            'git', 'show', '-s', '--date=format:%Y-%m-%d', '--format=%h (%cd)', 'HEAD',
+            'git',
+                '-C', os.path.dirname(__file__),
+            'show',
+                '-s',
+                '--date=format:%Y-%m-%d',
+                '--format=%h (%cd)',
+                'HEAD',
         ], text=True)
     except subprocess.CalledProcessError:
         if ci:
