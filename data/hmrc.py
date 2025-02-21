a73a9e87629b0e715374b99bbaaa4282007632b9
LateGenXer <127238994+LateGenXer@users.noreply.github.com>
2025-02-21 16:44:01 +0000

    Add typing annotations to hmrc module.

--- data/hmrc.py
+++ data/hmrc.py
@@ -19,7 +19,7 @@ _session = requests.Session()
 
 
 @functools.lru_cache
-def exchange_rates(year, month):
+def exchange_rates(year:int, month:int) -> dict[str, float]:
     assert year >= 2021
     assert 1 <= month and month <= 12
     url = f'https://www.trade-tariff.service.gov.uk/api/v2/exchange_rates/files/monthly_xml_{year}-{month}.xml'
@@ -29,15 +29,22 @@ def exchange_rates(year, month):
     stream = io.BytesIO(r.content)
     tree = xml.etree.ElementTree.parse(stream)
     root = tree.getroot()
-    rates = {}
+    rates: dict[str, float] = {}
     for node in root:
-        currency = node.find('currencyCode').text
-        rate = float(node.find('rateNew').text)
+        currencyCode = node.find('currencyCode')
+        assert currencyCode is not None
+        currency = currencyCode.text
+        assert currency is not None
+        rateNew = node.find('rateNew')
+        assert rateNew is not None
+        rateText = rateNew.text
+        assert rateText is not None
+        rate = float(rateText)
         rates[currency] = rate
     return rates
 
 
-def exchange_rate(currency):
+def exchange_rate(currency:str) -> float:
     today = datetime.datetime.now(datetime.timezone.utc).date()
     rates = exchange_rates(today.year, today.month)
     return rates[currency]
